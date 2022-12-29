7eda248aab467a262c5e05e9f363e5f74dbbbf82
Vicko Novianto <vicko1011@gmail.com>
2022-12-29 16:09:51 +0700

    implement functionality

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
+const multer  = require('multer') // package to handle file upload
+const upload = multer({ dest: './public/data/uploads/' })
 require('dotenv').config()
 
-var app = express();
+const app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
-
-
+// api to handle file upload
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => { // upfile is field name in the form in index.html
+  const filename = req.file.originalname
+  const filetype = req.file.mimetype
+  const filesize = req.file.size
+  res.json({ name: filename, type: filetype, size: filesize })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
